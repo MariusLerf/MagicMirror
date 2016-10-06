d5f839c207b65371a0671cb83410f910119bf60b
mariusl95 <marius.lerf@schulenspiez.ch>
2016-10-06 22:26:03 +0200

    Update waswarheute.js

--- waswarheute/waswarheute.js
+++ waswarheute/waswarheute.js
@@ -41,16 +41,6 @@ Module.register("waswarheute",{
 		self.feed.find( "#content ul" ).children().each( function(i, li) {
 			self.feed.find( "#content ul" ).prepend(li);
 		});
-		//Create Table
-		self.feed.find( "#content" ).append( "<table></table>" );	
-		self.feed.find( "#content ul li" ).each( function() {
-			var span = self.feed.find( this ).find( "span[title]" );
-			var t1 =  span.attr( "title" ).replace( "Heute v", "V0" ) + " (" + span.html() + "):" ;
-			span.remove();
-			var t2 = self.feed.find( this ).html().substring(13);
-			self.feed.find( "table" ).append("<tr><td>" + t1 + "</td><td>" + t2 + "</td></tr>");
-		});
-		self.feed.find( "ul" ).remove();
 	},
 
 	// Override dom generator.
