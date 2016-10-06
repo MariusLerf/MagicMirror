cb28468e018db5a2ddb7979bab5983fe1f804e3e
mariusl95 <marius.lerf@schulenspiez.ch>
2016-10-06 20:15:45 +0200

    Update waswarheute.js

--- waswarheute/waswarheute.js
+++ waswarheute/waswarheute.js
@@ -25,9 +25,31 @@ Module.register("waswarheute",{
 			success: function(data) {
 				self.feed = $( "<span><div id='content' class='xsmall'></div></span>" );
 				self.feed.find( "#content" ).append(data.responseData.feed.entries[data.responseData.feed.entries.length - 1].content);
+				cleanUp();
 				self.updateDom();
 			}
 		});
+	},
+	
+	cleanUp: function() {
+		//Remove image
+		feed.find( "#content img" ).parent().parent().remove();
+		/*//Remove links
+		$( "#content a" ).contents().unwrap();
+		//Reverse list order
+		$( "#content ul" ).children().each( function(i, li) {
+			$( "#content ul" ).prepend(li);
+		});
+		//Create Table
+		$( "#content" ).append( "<table></table>");	
+		$( "#content ul li" ).each( function() {
+			var span = $( this ).find( "span" );
+			var t1 =  span.attr( "title" ).replace( "Heute v", "V0" ) + " (" + span.html() + "):" ;
+			span.remove();
+			var t2 = $( this ).html().substring(13);
+			$( "table" ).append("<tr><td>" + t1 + "</td><td>" + t2 + "</td></tr>");
+		});
+		$( "ul" ).remove();*/
 	},
 
 	// Override dom generator.
