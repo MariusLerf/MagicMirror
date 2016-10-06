74caffd27961a9d68de1792640a6515fde0d57b9
mariusl95 <marius.lerf@schulenspiez.ch>
2016-10-06 21:55:21 +0200

    Update waswarheute.js

--- waswarheute/waswarheute.js
+++ waswarheute/waswarheute.js
@@ -46,7 +46,7 @@ Module.register("waswarheute",{
 		self.feed.find( "#content ul li" ).each( function() {
 			var span = self.feed.find( this ).find( "span" );
 			var t1 =  span.attr( "title" ).replace( "Heute v", "V0" ) + " (" + span.html() + "):" ;
-			span.remove();
+		//	span.remove();
 		//	var t2 = self.feed.find( this ).html().substring(13);
 		//	self.feed.find( "table" ).append("<tr><td>" + t1 + "</td><td>" + t2 + "</td></tr>");
 		});
