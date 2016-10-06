aa8de50aebb73d596f0869b9d85f792d8ebd83ce
mariusl95 <marius.lerf@schulenspiez.ch>
2016-10-06 18:40:22 +0200

    Update node_helper.js

--- waswarheute/node_helper.js
+++ waswarheute/node_helper.js
@@ -1,6 +1,13 @@
 var NodeHelper = require("node_helper");
 module.exports = NodeHelper.create({
+	
+	i: 0,
+	
 	start: function() {
-		this.sendSocketNotification("TEXT", "heyhey");
+		var self = this
+		setInterval(function() {
+			self.sendSocketNotification("TEXT", "heyhey " + i);
+			i++;
+		}, 1000);
 	},
 });
