06f9381d561665b692ee90a7c61c201220ec36b9
mariusl95 <marius.lerf@schulenspiez.ch>
2016-10-06 20:02:49 +0200

    Update waswarheute.js

--- waswarheute/waswarheute.js
+++ waswarheute/waswarheute.js
@@ -9,7 +9,7 @@ Module.register("waswarheute",{
 		feedURL: ""
 	},
 	
-	feed: $( "<div id='content' class='small'></div>" ),
+	feed: $( "<div id='content' class='small'>asdf</div>" ),
 	
 	getScripts: function() {
 		return [
@@ -23,15 +23,10 @@ Module.register("waswarheute",{
 			url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent("https://de.wikipedia.org/w/api.php?action=featuredfeed&feed=onthisday&feedformat=atom"),
 			dataType: 'json',
 			success: function(data) {
-				self.feed.find( "#content" ).append(data.responseData.feed.entries[data.responseData.feed.entries.length - 1].content);
-				self.updateDom();
+				//self.feed.find( "#content" ).append(data.responseData.feed.entries[data.responseData.feed.entries.length - 1].content);
+				//self.updateDom();
 			}
 		});
-	},
-	
-	getList: function(url) {
-		var content = "xx";
-		return(content);
 	},
 
 	// Override dom generator.
