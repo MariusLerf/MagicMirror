0ccde942a040ff7a733dd20cb0e406a1cb865256
mariusl95 <marius.lerf@schulenspiez.ch>
2016-10-07 22:32:50 +0200

    Update bandsintown.js

--- bandsintown/bandsintown.js
+++ bandsintown/bandsintown.js
@@ -28,7 +28,7 @@ Module.register("bandsintown",{
 	//First update & set interval to 1h
 	start: function() {
 		this.updateList();
-		setInterval(this.updateDom(), 1000);
+		setInterval(function() {this.updateDom()}, 1000);
 	},
 
 	// Override dom generator.
