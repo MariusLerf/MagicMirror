9f74e0a6bd1fe4e453d04d74bea5a1c620e63b76
mariusl95 <marius.lerf@schulenspiez.ch>
2016-10-07 22:19:09 +0200

    Update bandsintown.js

--- bandsintown/bandsintown.js
+++ bandsintown/bandsintown.js
@@ -34,5 +34,60 @@ Module.register("bandsintown",{
 		var wrapper = document.createElement("div");
     		wrapper.innerHTML = "hi";
 		return wrapper;
-	}
+	},
+	
+	
+	
+	
+	
+	//Functions
+	/////////////////////////////////////
+	
+	update: function() {
+		var self = this;
+		self.eventList = [];
+		self.errorList = [];
+		self.noGigs = [];
+		self.updateCounter = 0;
+		
+		self.config.bands.forEach( function(band) {
+			var url = "";
+			if (self.config.searchRadius == 0) url = "http://api.bandsintown.com/artists/" + band + "/events/search.json?api_version=2.0&app_id=MagicMirror&location=" + self.config.location;
+			else url = "http://api.bandsintown.com/artists/" + band + "/events/search.json?api_version=2.0&app_id=MagicMirror&location=" + self.config.location + "&radius=" + self.config.searchRadius;
+			$.ajax({
+				url: url,
+				dataType: 'jsonp',
+				
+				success: function(events){
+					self.updateCounter++;
+					if (events.hasOwnProperty("errors")) {
+						self.errorList.push({errorBand: band, errorText: events.errors[0]});
+					}
+					else if (events.length == 0) {
+						self.noGigs.push(band);
+					}
+					else {
+						events.forEach( function(event) {
+							self.addEvent(event);
+						});
+					}
+					if (self.updateCounter == self.config.bands.length) {
+						self.finishUpdate();
+					}
+				},
+				
+				error: function(arg1, arg2) {
+					self.updateCounter++;
+					if (self.updateCounter == self.config.bands.length) {
+						self.finishUpdate();
+					}
+				}
+			});
+		});
+		
+		
+	},
+	
+	
+	
 });
