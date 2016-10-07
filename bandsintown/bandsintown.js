3f36821a2c7d14e14ce888323194f4711f941620
mariusl95 <marius.lerf@schulenspiez.ch>
2016-10-07 22:10:12 +0200

    Update bandsintown.js

--- bandsintown/bandsintown.js
+++ bandsintown/bandsintown.js
@@ -1,1 +1,27 @@
+/* Magic Mirror
+ * Module: WasWarHeute
+ */
 
+Module.register("waswarheute",{
+
+	// Default module config.
+	defaults: {
+		feedURL: "https://de.wikipedia.org/w/api.php?action=featuredfeed&feed=onthisday&feedformat=atom"
+	},
+	
+	getScripts: function() {
+		return [
+			'http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'
+		]
+	},
+	
+	start: function() {
+	},
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+    wrapper.innerHTML = "hi";
+		return wrapper;
+	}
+});
