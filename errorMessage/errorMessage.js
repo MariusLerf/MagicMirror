01344c7b78b2a68490889e5268e07889874ea88d
mariusl95 <marius.lerf@schulenspiez.ch>
2016-10-09 21:28:13 +0200

    Create errorMessage.js

---
+++ errorMessage/errorMessage.js
@@ -0,0 +1,30 @@
+/* Magic Mirror
+ * Module: WasWarHeute
+ */
+
+Module.register("errorMessage",{
+
+	// Default module config.
+	defaults: {
+	},
+	
+	getScripts: function() {
+		return [
+			'http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'
+		]
+	},
+  
+  getStyles: function() {
+		return ["errorMessage.css"];
+	},
+	
+	start: function() {
+	},
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+    
+		return wrapper;
+	}
+});
