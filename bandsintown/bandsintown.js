9c651871621de10b803a8f7aa95bd057b0fa167c
mariusl95 <marius.lerf@schulenspiez.ch>
2016-10-09 21:48:19 +0200

    Update bandsintown.js

--- bandsintown/bandsintown.js
+++ bandsintown/bandsintown.js
@@ -87,6 +87,7 @@ Module.register("bandsintown",{
 					self.firstUpdate = true;
 					if (self.updateCounter == self.config.bands.length) {
 						self.updateDom(fade);
+						self.sendNotification("SHOW_ALERT", {type: "notification", title: "test", message: "heyhey"}); 
 					}
 				},
 				
