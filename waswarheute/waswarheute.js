0d7272104b65c8421552813cd21069decb03d218
mariusl95 <marius.lerf@schulenspiez.ch>
2016-10-13 19:55:27 +0200

    Update waswarheute.js

--- waswarheute/waswarheute.js
+++ waswarheute/waswarheute.js
@@ -36,6 +36,7 @@ Module.register("waswarheute",{
 			url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(self.config.feedURL),
 			dataType: 'json',
 			success: function(data) {
+				self.feed = "";
 				self.feed = $( "<span><div id='content' class='xsmall bright'></div></span>" );
 				self.feed.find( "#content" ).append(data.responseData.feed.entries[data.responseData.feed.entries.length - 1].content);
 				//self.data.header = data.responseData.feed.entries[data.responseData.feed.entries.length - 1].title;
